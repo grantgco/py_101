2bf059cba50446dfe380779966d22e73319f049c
grantgreeson <101661770+grantgreeson@users.noreply.github.com>
2023-11-19 17:23:05 -0500

    Initial

---
+++ loops_practice.py
@@ -0,0 +1,15 @@
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+# print("This can be so much easier with loops!")
+
+string = "This can be so much easier with loops!"
+
+for i in range(10):
+    print(string + " " + str(i))
