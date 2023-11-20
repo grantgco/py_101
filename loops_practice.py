1865b6080dce1b61c451a1d5b577e820925f2549
grantgco <101661770+grantgco@users.noreply.github.com>
2023-11-20 11:18:30 -0500

    Update loops_practice.py

--- loops_practice.py
+++ loops_practice.py
@@ -13,3 +13,10 @@ string = "This can be so much easier with loops!"
 
 for i in range(10):
     print(string + " " + str(i))
+
+
+# Simple and a bit more elegant with in line...
+
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+can_ride_coaster = [num for num in heights if num > 161]
+print(can_ride_coaster)
