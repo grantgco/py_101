1295cd4a58da88776f149e13b4e893dc3a5552fe
grantgreeson <101661770+grantgreeson@users.noreply.github.com>
2023-11-20 11:11:40 -0500

    Updated with new practice

--- loops_practice.py
+++ loops_practice.py
@@ -13,3 +13,10 @@ string = "This can be so much easier with loops!"
 
 for i in range(10):
     print(string + " " + str(i))
+
+
+# Modifying Lists Elegantly
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
