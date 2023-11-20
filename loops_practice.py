89697d634e568cafb44b0e4a75000c1b7b51aa53
grantgco <101661770+grantgco@users.noreply.github.com>
2023-11-20 11:22:31 -0500

    Update loops_practice.py

--- loops_practice.py
+++ loops_practice.py
@@ -15,8 +15,7 @@ for i in range(10):
     print(string + " " + str(i))
 
 
-# Simple and a bit more elegant with in line...
-
-heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
-can_ride_coaster = [num for num in heights if num > 161]
-print(can_ride_coaster)
+# Modifying Lists Elegantly
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
