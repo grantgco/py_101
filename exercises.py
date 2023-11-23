8ff8d54a9f54cbb4359dc9d2590339980b462310
grantgreeson <101661770+grantgreeson@users.noreply.github.com>
2023-11-23 09:19:13 -0500

    Initial

---
+++ exercises.py
@@ -0,0 +1,18 @@
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+
+print(author_names)
+
+counter = 0
+for author in author_names:
+    author.split()
+
+# for author in author_names:
+#     split_authors.append(author.split())
+
+# print(split_authors)
+
+# authors_2 = split_authors[]
+
+
