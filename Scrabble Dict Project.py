edf55189a7058eeb1344e22a2a8d31c8921c32c8
grantgreeson <101661770+grantgreeson@users.noreply.github.com>
2023-11-25 15:58:09 -0500

    Initial project completed

---
+++ Scrabble Dict Project.py
@@ -0,0 +1,41 @@
+# Scrabble Project
+# November 25, 2023
+# PGG
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 0]
+
+letters_to_points = dict(zip(letters,points))
+
+def score_word(word):
+    point_total = 0
+    upperword = word.upper()
+    for letter in upperword:
+        point_total += letters_to_points.get(letter)
+    return point_total
+
+# print(score_word("BROWNIE"))  #Should produce 15... and it does.
+
+player1 = ["BLUE", "TENNIS", "EXIT"]
+word_nerd = ["EARTH", "EYES", "MACHINE"]
+lexiCon = ["ERASER", "BELLY", "HUSKY"]
+ProfReader = ["ZAP", "COMA", "PERIOD"]
+
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "word_nerd": word_nerd, "lexicon": lexiCon, "ProfReader": ProfReader}
+
+#print(player_to_words)
+
+player_to_points = dict()
+
+# Update Point Totals
+for player, words in player_to_words.items():
+  player_points=0
+  for word in words:
+     player_points += score_word(word)
+  player_to_points[player] = player_points
+
+print(player_to_points)
+
+def play_word(word, player):
+   player_to_words[player].append(upper(word))
+
---
+++ dictionary_practice.py
@@ -0,0 +1,43 @@
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+
+
+# Using the dictionary .values method to return values.
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0 
+
+print(num_exercises.values())
+
+x = 0
+for i in num_exercises:
+  total_exercises += list(num_exercises.values())[x] # This converts this to a list first then iterates in each value of the list
+  x += 1
+
+print(total_exercises)
+
+
+# Using the .items to return two values - the key and corresponding value as two items
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for key, value in pct_women_in_occupation.items():
+  print("Women make up " + str(value) + " percent of " + key +"s")
+
+
+# Tarot Project to put it together:
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your " + key + " is the " + value + " card.")
